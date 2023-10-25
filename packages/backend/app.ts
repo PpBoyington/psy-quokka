d8e231d394493ab7fb3b4237f4e364094e0d95e1
Gregory <gregory.delfrate@gmail.com>
2023-10-25 14:22:19 +0200

    feature: [HQ-36] create account init mono repo

---
+++ packages/backend/app.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from 'cors'
+
+const PORT = 8080;
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/api/home", (req, res) => {
+  res.json({ message: "Hello world!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
