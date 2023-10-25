95d609f8f32b49b663320543d06a13b94542da96
Gregory <gregory.delfrate@gmail.com>
2023-10-25 17:12:33 +0200

    feature: [HQ-36] create account eslint + prettier config remove useless code add mui deps

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./packages/*/tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
